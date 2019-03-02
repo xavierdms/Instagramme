3ed540f77d1edbc5d6852b3599597e027463a7c8
xavierdms <id5@humboldt.edu>
2019-03-02 15:47:47 -0800

    Initial commit - login screen

---
+++ app/src/main/java/com/xdms/instagramme/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.xdms.instagramme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                login(username,password);
+            }
+        });
+    }
+
+    private void login(String username, String password){
+        //TODO: navigate to new activity if the user has signed properly
+    }
+}
