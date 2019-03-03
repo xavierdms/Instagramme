799e41ab4a6b30de70a2d83925808bc066acefb7
xavierdms <id5@humboldt.edu>
2019-03-02 19:48:52 -0800

    user can login

--- app/src/main/java/com/xdms/instagramme/LoginActivity.java
+++ app/src/main/java/com/xdms/instagramme/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.xdms.instagramme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
 
     private EditText etUsername;
     private EditText etPassword;
@@ -31,6 +39,22 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login(String username, String password){
-        //TODO: navigate to new activity if the user has signed properly
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with login");
+                    e.printStackTrace();
+                    return;
+                }
+                //TODO: navigate to new activity if the user has signed properly
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
     }
 }
