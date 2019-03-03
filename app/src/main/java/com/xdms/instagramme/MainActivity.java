b29e7e65cfe464e3271f2b2ad1b129bcf9111077
xavierdms <id5@humboldt.edu>
2019-03-02 20:37:41 -0800

    create post layout added

--- app/src/main/java/com/xdms/instagramme/MainActivity.java
+++ app/src/main/java/com/xdms/instagramme/MainActivity.java
@@ -1,13 +1,32 @@
 package com.xdms.instagramme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etDescription = findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        queryPosts();
+    }
+
+    private void queryPosts() {
+
     }
 }
